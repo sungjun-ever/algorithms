96ee29a76a2ac38a587ec0951331228e8f61122a
임성준 <zkfmak10@gmail.com>
2023-02-18 16:33:40 +0900

    leetcode 06

--- leetcode6.php
+++ leetcode6.php
@@ -1,1 +1,30 @@
 <?php
+class Solution {
+
+    /**
+     * @param String $s
+     * @param Integer $numRows
+     * @return String
+     */
+    function convert($s, $numRows) {
+        if ($numRows <= 1 || strlen($s) <= $numRows) return $s;
+        $ans = array_fill(0, $numRows, "");
+        $n = strlen($s);
+        $step = 2 * ($numRows - 1);
+        for($i=0; $i<$n; $i++){
+            $index = $i % $step;
+            if($index < $numRows){
+                $ans[$index] .= $s[$i];
+            } else {
+                $ans[$step - $index] .= $s[$i];
+            }
+        }
+        return implode('', $ans);
+    }
+}
+
+$s = "PAYPALISHIRING";
+$numRows = 3;
+$sol = new Solution();
+$result = $sol->convert($s, $numRows);
+print_r($result);
