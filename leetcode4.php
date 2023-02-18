a425da2420b34b5be9217843a692cae5403d395f
임성준 <zkfmak10@gmail.com>
2023-02-18 15:44:40 +0900

    leetcode 04

--- leetcode4.php
+++ leetcode4.php
@@ -7,6 +7,20 @@ class Solution {
      * @return Float
      */
     function findMedianSortedArrays($nums1, $nums2) {
+        $arr = array_merge($nums1, $nums2);
+        sort($arr);
+        $count = count($arr);
+        if($count % 2 == 0){
+            return ($arr[($count / 2) -1] + $arr[$count / 2]) / 2;
+        }
 
+        if($count % 2 == 1){
+            return $arr[$count / 2];
+        }
     }
 }
+
+$arr1 = [1, 5, 7];
+$arr2 = [2, 8];
+$sol = new Solution();
+$sol->findMedianSortedArrays($arr1, $arr2);
